e8a65acfd7c2d14de2348a4d796354d6a96fbacb
kajetan.swiatek <kajetan.swiatek@aptitudesoftware.com>
2020-11-01 19:11:54 +0100

    styled form card

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, text)
+import Html exposing (Html, button, div, form, h1, text)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 import Survey exposing (..)
@@ -56,16 +56,22 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ div [] [ text model.current.text ]
-        , button
-            [ onClick PreviousQuestion
-            , class "bg-yellow-500 text-black font-bold py-2 px-4"
+    div [ class "w-full max-w-md" ]
+        [ div
+            [ class "bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col items-center" ]
+            [ h1 [ class "text-xl inline-block" ] [ text model.current.text ]
+            , div
+                [class "flex flex-row w-full"]
+                [ button
+                    [ onClick PreviousQuestion
+                    , class "bg-yellow-500 text-black font-bold py-2 px-4 w-3/6 mr-2 rounded"
+                    ]
+                    [ text "◀ Previous" ]
+                , button
+                    [ onClick NextQuestion
+                    , class "bg-yellow-500 text-black font-bold py-2 px-4 w-3/6 ml-2 rounded"
+                    ]
+                    [ text "Next ▶" ]
+                ]
             ]
-            [ text "◀ Previous" ]
-        , button
-            [ onClick NextQuestion
-            , class "bg-yellow-500 text-black font-bold py-2 px-4"
-            ]
-            [ text "Next ▶" ]
         ]
